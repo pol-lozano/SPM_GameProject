b67c5a4788d274d2940c3d5f04ce468f98b666c1
henrysmitt <rickardsmitt@gmail.com>
2021-04-16 16:49:25 +0200

    GM

---
+++ Assets/Scripts/Utilities/CheckPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public static Transform currentCheckPoint;
+
+
+    /*DEN BORDE INTE AKTIVERAS SÅ HÄR, SPELAREN BORDE GÅ FRAM OCH INTERAGERA*/
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            currentCheckPoint = this.transform;
+            //call CheckPointEvent. enemies respawns and player resets
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/GameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    [SerializeField] private GameObject player;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+    }
+
+
+    void ReloadGame()
+    {
+        /*
+         play Fade Out effects:
+             - fadeOut animation
+             - play sounds
+             - particles?
+         */
+
+
+        //Reload the scene with enemies and all
+
+
+        //Resets the player to latest checkpoint
+        player.transform.position = CheckPoint.currentCheckPoint.position;
+
+
+        //Reset player health and stamina 
+        //player.GetComponent<AttributeSystem>().ResetAttributes();
+
+
+        /*
+         play Fade In effects:
+             - fadeIn animation
+             - play sounds
+             - particles?
+         */
+    }
+}
