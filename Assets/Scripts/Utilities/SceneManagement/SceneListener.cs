de5b00abc9887bd3c3fe4481f897fe9fde801831
henrysmitt <rickardsmitt@gmail.com>
2021-04-24 13:55:55 +0200

    smågrejer

---
+++ Assets/Scripts/Utilities/SceneManagement/SceneListener.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneListener : MonoBehaviour
+{
+    public System.Action func;
+    
+
+}
--- Assets/Scripts/Utilities/SceneManagement/SceneLoader.cs
+++ Assets/Scripts/Utilities/SceneManagement/SceneLoader.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class SceneLoader : MonoBehaviour
 {
 
     [SerializeField] private string sceneToLoad;
+    //private UnityEvent sceneTrigger;
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            //sceneTrigger.Invoke();
             Debug.Log("Load SCENE");
             SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
         }
+
+        
+
+        
         
     }
+
+
 }
