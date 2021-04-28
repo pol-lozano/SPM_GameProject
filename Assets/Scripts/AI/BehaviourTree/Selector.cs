d1a40c8cd2136f88d2113e86e456305b2afe5cab
Sajid <81232634+SajidMasoud@users.noreply.github.com>
2021-04-28 16:52:56 +0200

    Revert "Merge remote-tracking branch 'origin/Rickard' into PathB"
    This reverts commit 5e3c41aa23fd512096bc8752774aec2715362254, reversing
    changes made to 50304e882cea0979bc93984e26694e699169ebad.

--- Assets/Scripts/AI/BehaviourTree/Selector.cs
+++ Assets/Scripts/AI/BehaviourTree/Selector.cs
@@ -7,45 +7,31 @@ public class Selector : Node
 
     protected List<Node> nodes = new List<Node>();
 
-    private Decorator decorator = null;
-
 
     public Selector(List<Node> list)
     {
         nodes = list;
     }
 
-    public Selector(List<Node> list, Decorator d)
-    {
-        nodes = list;
-        decorator = d;
-    }
-
     public override NODE_STATE Evaluate()
     {
-        if (decorator.Condition() || decorator == null)
+        foreach (var node in nodes)
         {
-            foreach (var node in nodes)
+            switch (node.Evaluate())
             {
-                switch (node.Evaluate())
-                {
-                    case NODE_STATE.SUCCESS:
-                        nodeState = NODE_STATE.SUCCESS;
-                        return nodeState;
-                    case NODE_STATE.RUNNING:
-                        nodeState = NODE_STATE.RUNNING;
-                        return nodeState;
-                    case NODE_STATE.FAILURE:
-                        break;
-                    default:
-                        break;
-                }
+                case NODE_STATE.SUCCESS:
+                    nodeState = NODE_STATE.SUCCESS;
+                    return nodeState;
+                case NODE_STATE.RUNNING:
+                    nodeState = NODE_STATE.RUNNING;
+                    return nodeState;
+                case NODE_STATE.FAILURE:
+                    break;
+                default:
+                    break;
             }
-            nodeState = NODE_STATE.FAILURE;
-            return nodeState;
         }
-        else
-            return NODE_STATE.FAILURE;
-        
+        nodeState = NODE_STATE.FAILURE;
+        return nodeState;
     }
 }
