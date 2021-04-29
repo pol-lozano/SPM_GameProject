4d17d4721aaba576bf5f936e3091e1796cf70412
henrysmitt <rickardsmitt@gmail.com>
2021-04-29 11:09:14 +0200

    knockback

--- Assets/Scripts/AI/AIStates/EnemyState.cs
+++ Assets/Scripts/AI/AIStates/EnemyState.cs
@@ -22,7 +22,10 @@ public abstract class EnemyState : State
         //VI kanske kan lägga stun här men då måste stunState vara båda för båda fienderna?
         //if (AIController.isStunned) stateMachine.Transition<StunState>();
         //lägg in en HitState som gör typ knockback?
-        if(AIController.HealthComponent.CurrentHealth <= 0) { stateMachine.Transition<EnemyDeadState>(); }
+        if (AIController.HealthComponent.Invulnerable && AIController.HealthComponent.LastType != typeof(Projectile))
+            stateMachine.Transition<EnemyKnockBackState>();
+        if(AIController.HealthComponent.CurrentHealth <= 0) 
+                stateMachine.Transition<EnemyDeadState>(); 
     }
 
     protected bool CanSeePlayer()
