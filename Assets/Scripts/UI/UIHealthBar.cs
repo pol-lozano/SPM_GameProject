729c3120680f954de60bd18a31b3dd16124ab8fb
electfall70 <llop2000@gmail.com>
2021-05-02 16:38:09 +0200

    updated health bar a bit

--- Assets/Scripts/UI/UIHealthBar.cs
+++ Assets/Scripts/UI/UIHealthBar.cs
@@ -13,37 +13,37 @@ public class UIHealthBar : MonoBehaviour
 
     private bool activated;
     private float width;
-    private RectTransform parent;
+    private RectTransform rectParent;
 
-    private void Awake() => Initialize();
-
-    private void Initialize()
+    private void Awake()
     {
-        parent = GetComponent<RectTransform>();
+        rectParent = GetComponent<RectTransform>();
         Deactivate();
-    }
-
-    void LateUpdate()
-    {
-        Vector3 direction = (target.position - Camera.main.transform.position).normalized;
-
-        if (activated)
-        {
-            bool isBehind = Vector3.Dot(direction, Camera.main.transform.forward) <= 0.0f;
-            UIElements.SetActive(!isBehind);
-        }
-
-        transform.position = Camera.main.WorldToScreenPoint(target.position + offset);
-
-        float newWidth = Mathf.Lerp(damageTakenImage.rectTransform.rect.width, width, 1.5f * Time.deltaTime);
-        damageTakenImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
     }
 
     public void SetHealthBarPercentage(float percentage)
     {
-        float parentWidth = parent.rect.width;
+        float parentWidth = rectParent.rect.width;
         width = parentWidth * percentage;
         currentHealthImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+    }
+
+    void LateUpdate()
+    {
+        if (activated)
+            UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        transform.position = Camera.main.WorldToScreenPoint(target.position + offset);
+
+        Vector3 direction = (target.position - Camera.main.transform.position).normalized;
+        bool isBehind = Vector3.Dot(direction, Camera.main.transform.forward) <= 0.0f;
+        UIElements.SetActive(!isBehind);
+
+        float newWidth = Mathf.Lerp(damageTakenImage.rectTransform.rect.width, width, 1.5f * Time.deltaTime);
+        damageTakenImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
     }
 
     public void Activate()
