5b07bfef6769c0a808dba1b9c5e5e2d608da8cd5
henrysmitt <rickardsmitt@gmail.com>
2021-05-12 11:29:59 +0200

    BT

---
+++ Assets/Scripts/AI/BehaviourTree/BehaviourTrees/BT_UmbralMoth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BT_UmbralMoth : BehaviourTree
+{
+
+
+
+    public override void ConstructBehaviourTree()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RunBehaviourTree()
+    {
+        throw new System.NotImplementedException();
+    }
+}
