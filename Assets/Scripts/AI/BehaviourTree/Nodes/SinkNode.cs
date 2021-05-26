13f7eb4b358c175563060bac26925453382e4f37
henrysmitt <rickardsmitt@gmail.com>
2021-05-26 10:57:47 +0200

    better sink

--- Assets/Scripts/AI/BehaviourTree/Nodes/SinkNode.cs
+++ Assets/Scripts/AI/BehaviourTree/Nodes/SinkNode.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class SinkNode : Node
 {
+    float acceleration = 0;
     public SinkNode(BehaviourTree tree)
     {
         this.tree = tree;
@@ -15,11 +16,13 @@ public class SinkNode : Node
         BlackBoard.Agent.ResetPath();
         if (BlackBoard.Agent.baseOffset > 1.21f && BlackBoard.Rising == false)
         {
-            BlackBoard.Agent.baseOffset -= Time.deltaTime * BlackBoard.StunSpeed;
+            BlackBoard.Agent.baseOffset -= Time.deltaTime * BlackBoard.StunSpeed + acceleration;
+            acceleration += 0.01f;
             return NODE_STATE.RUNNING;
         }
         else
         {
+            acceleration = 0;
             return NODE_STATE.SUCCESS;
         }
     }
