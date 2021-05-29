ad53289c28f9146beb58e92b3541d5daa7cee63d
henrysmitt <rickardsmitt@gmail.com>
2021-05-29 14:16:25 +0200

    ljud... lite mesigt men ok

--- Assets/Scripts/Puzzles/FloorTrap.cs
+++ Assets/Scripts/Puzzles/FloorTrap.cs
@@ -6,11 +6,14 @@ public class FloorTrap : MonoBehaviour
 {
 
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioData audioData;
+    [SerializeField] private AudioSource audioSource;
     [SerializeField] private ShakeEventData shakeData;
 
     public void OnTriggerEnter(Collider other)
     {
         animator.SetTrigger("Standing");
+        EventHandler<SoundEvent>.FireEvent(new SoundEvent(audioData, audioSource));
         EventHandler<ShakeEvent>.FireEvent(new ShakeEvent(shakeData));
 
 
