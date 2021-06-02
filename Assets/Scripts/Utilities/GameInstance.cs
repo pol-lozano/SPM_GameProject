30fea2cfedb3ca4f2dd75197b7aab411e9c2d94a
henrysmitt <rickardsmitt@gmail.com>
2021-06-02 10:38:09 +0200

    load fix

--- Assets/Scripts/Utilities/GameInstance.cs
+++ Assets/Scripts/Utilities/GameInstance.cs
@@ -3,15 +3,29 @@ using UnityEngine;
 
 public class GameInstance : MonoBehaviour
 {
-    private void OnEnable() => EventHandler<ReloadEvent>.RegisterListener(Reload);
-    private void OnDisable() => EventHandler<ReloadEvent>.UnregisterListener(Reload);
+    private void OnEnable()
+    {
+        EventHandler<ReloadEvent>.RegisterListener(Reload);
+        EventHandler<DyingEvent>.RegisterListener(OnDying);
+    }
+    private void OnDisable()
+    {
+        EventHandler<ReloadEvent>.UnregisterListener(Reload);
+        EventHandler<DyingEvent>.UnregisterListener(OnDying);
+    }
 
     private void Reload(ReloadEvent eve)
     {
         CharacterController3D.Player.transform.position = Checkpoint.currentCheckPoint.transform.position;
         CharacterController3D.Player.GetComponent<HealthComponent>().ResetHealth();
-        EnemyLoader.ResetEnemies();
-        EventHandler<ReloadEnemiesEvent>.FireEvent(new ReloadEnemiesEvent());
+        //EventHandler<ReloadEnemiesEvent>.FireEvent(new ReloadEnemiesEvent());
+    }
+
+    private void OnDying(DyingEvent data)
+    {
+        HealthComponent hc = (HealthComponent)data.Info.hitComponent;
+        if (hc.IsPlayer)
+            EnemyLoader.ResetEnemies();
     }
 
 
